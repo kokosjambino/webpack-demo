ba501964a9b707af2eb04e94dcee99c1aca23464
Kostya7375 <78868972+Kostya7375@users.noreply.github.com>
2023-02-27 20:18:36 +0400

    feat: added webpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
 entry: {
     index: './src/index.js',
     print: './src/print.js',
       },
+    plugins: [
+    new HtmlWebpackPlugin({
+        title: 'Output Management',
+    }),
+    ],
   output: {
     // clean: true, 
     filename: '[name].bundle.js',
