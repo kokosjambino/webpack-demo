79e8ce84b9995950767a0d0d337150367622653f
Kostya7375 <78868972+Kostya7375@users.noreply.github.com>
2023-02-27 20:29:53 +0400

    feat: added test error and find error file

--- webpack.config.js
+++ webpack.config.js
@@ -3,30 +3,33 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
+mode: 'development',
 entry: {
     index: './src/index.js',
     print: './src/print.js',
-      },
+    },
+    devtool: 'inline-source-map',
     plugins: [
     new HtmlWebpackPlugin({
-        title: 'Output Management',
+        title: 'Development',
     }),
     ],
-  output: {
+output: {
     // clean: true, 
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
+    clean: true,
+},
+module: {
     rules: [
-      {
+        {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
-      },
-      {
+        },
+        {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
-      },
+        },
     ],
-  },
+},
 };
